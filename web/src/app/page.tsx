3cc64017723aa398cc2c30060851fc7bf6e26a72
Lucas Monteiro <77646992+lucas752@users.noreply.github.com>
2023-05-18 18:57:12 -0300

    criando projeto base web

---
+++ web/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="text-2xl font-bold uppercase w-36 h-20 bg-red-500 rounded m-10 color text-white">
+      SpaceTime
+    </div>
+  )
+}
