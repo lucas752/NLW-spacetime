8a22dfcefee52f9c3883be7749c8944b44adca7d
Lucas Monteiro <77646992+lucas752@users.noreply.github.com>
2023-05-22 16:38:21 -0300

    criando botão de realizar cadastro

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,5 @@
+import { User } from "lucide-react";
+
 export default function Home() {
   return (
     <main className="grid grid-cols-2 min-h-screen">
@@ -5,6 +7,15 @@ export default function Home() {
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 bg-purple-700 opacity-50 rounded-full blur-full"/>
 
         <div className="absolute top-0 bottom-0 w-2 bg-stripes right-2"></div>
+
+        <a href="" className="flex items-center gap-3 text-left hover:text-gray-50 transition-colors">
+          <div className="flex h-10 w-10 item items-center justify-center rounded-full bg-gray-400">
+            <User className="h-5 w-5 text-gray-500"/>
+          </div>
+          <p className="text-sm leading-snug max-w-[140px]"> 
+            <span className="underline">Crie sua conta</span> e salve suas memórias!
+          </p>
+        </a>
       </div>
 
       <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
