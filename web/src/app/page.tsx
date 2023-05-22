3c802ace4be025c97795b2c366b1a7d4f03ff1cd
Lucas Monteiro <77646992+lucas752@users.noreply.github.com>
2023-05-22 16:27:25 -0300

    criando base para a tela inicial

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,21 @@
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-950 text-zinc-50 p-6">
-      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
-      <h1 className="text-4xl font-bold font-alt">Sua cápsula do tempo</h1>
-    </div>
+    <main className="grid grid-cols-2 min-h-screen">
+      <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover">
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 bg-purple-700 opacity-50 rounded-full blur-full"/>
+
+        <div className="absolute top-0 bottom-0 w-2 bg-stripes right-2"></div>
+      </div>
+
+      <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="text-center leading-relaxed w-[360px]">
+            Você ainda não registrou nenhuma lembrança, comece a{'  '}
+            <a href="" className="underline hover:text-gray-50">criar agora</a>
+            !
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
