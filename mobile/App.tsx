aeebd0468f5fbf40e0ca7e5e3b652072fb34676a
Lucas Monteiro <77646992+lucas752@users.noreply.github.com>
2023-05-23 17:29:32 -0300

    Configurando fontes do mobile]

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree';
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold
+  })
+
+  if(!hasLoadedFonts) {
+    return null;
+  }
+
   return (
-    <View className="bg-gray-950 flex-1 items-center justify-center">
-      <Text className='text-zinc-50 font-bold text-5xl'>Hello world!</Text>
+    <View className="bg-gray-900 flex-1 items-center justify-center">
+      <Text className='text-gray-50 text-5xl font-alt'>Hello world!</Text>
+      <Text className='text-gray-50 text-5xl font-body'>Hello world!</Text>
       <StatusBar style="light" translucent/>
     </View>
   );
